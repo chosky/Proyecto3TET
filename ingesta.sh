18780aa62105b6f4348aa4703044393d7599e83d
chosky <davidhenaoo98@gmail.com>
2020-12-07 11:29:59 -0500

    se mejora ingesta para que vaya al raw

--- ingesta.sh
+++ ingesta.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 wget -o proyectoFinalCovid.csv 'https://raw.githubusercontent.com/chosky/Proyecto3TET/main/dataset/covid.csv'
-aws s3 cp proyectoFinalCovid.csv s3://trabajofinaltet/covid.csv
+aws s3 cp proyectoFinalCovid.csv s3://trabajofinaltet/raw/covid.csv
